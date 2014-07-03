5d8db78052a8290a3fc7fedb949681eb8b44d9ba
Thapelo-Tsotetsi <thapelo.tsotetsi504@gmail.com>
2014-07-03 16:13:35 +0200

    added blocks services

--- public/js/services.js
+++ public/js/services.js
@@ -37,3 +37,37 @@ services.factory('shapes', [function () {
         circle: circle
     };
 }]);
+
+services.factory('blocks', [function () {
+    function state(opts){
+        opts = opts || {};
+        var color = opts.color || '#33cc33';
+
+        function component(selection){
+            //entering
+            var enter = selection.enter().append('g')
+                        .attr('class','state');
+
+                enter.append('foreignObject')
+                    .attr('width', '100')
+                    .attr('height', '50')
+                    .attr('requiredExtensions', 'http://www/w3.org/1999/xhtml');
+
+                enter.append('text')
+                    .text(function(d){return d.name;})
+                    .style('fill',color);
+
+            //updating
+                    selection
+                    .attr('transform', 'translate (80, 0)');
+
+            //exiting
+            selection.exit()
+                .remove();
+        }
+        return component;
+    }
+    return {
+        state: state
+    };
+}]);
